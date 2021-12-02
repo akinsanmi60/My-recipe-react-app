8b57fe886c94e2cdd078e8ba661f60ca9de3cd38
Akinsanmi Akintunde <90307436+akinsanmi60@users.noreply.github.com>
2021-12-02 10:22:04 +0100

    Adding CSS styling to h1
    Reducing the margin-bottom

--- home/src/Recipe.js
+++ home/src/Recipe.js
@@ -4,7 +4,7 @@ import style from './recipes.module.css'
 const Recipe = ({title, calories, image, ingredients}) => { //the props is gotten from the props passed to the recipe in the app 
     return ( // details to be display about a recipe
         <div className={style.recipes}> 
-            <h1>{title}</h1>
+            <h1 className={style.h1}>{title}</h1>
             <p><b>Calories: </b>{calories}</p>
             <img src={image} alt="" className={style.image} />
             <ul className={style.ul}><b>Ingredients</b>{ingredients.map(ingredient =>(
